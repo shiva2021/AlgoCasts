c36de2060dfc003934375d47d841eb31e87b753c
Shivang Chaturvedi <shiva.chaturvedi2191@outlook.com>
2021-04-18 21:34:53 +0530

    maxChar solution

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,24 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  let obj = {};
+  let max = 0;
+  let maxChar = '';
+
+  for (let d of str) {
+    // !Object.prototype.hasOwnProperty.call(obj, d) ? obj[d] = 1 : obj[d]++
+    obj[d] = obj[d] + 1 || 1;
+  }
+
+  for (let key in obj) {
+    if (obj[key] > max) {
+      max = obj[key];
+      maxChar = key;
+    }
+  }
+
+  return maxChar
+}
 
 module.exports = maxChar;
