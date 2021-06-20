ef452f1cb1b839949ab78699336ebe67beeca8cb
Shivang Chaturvedi <shiva.chaturvedi2191@outlook.com>
2021-06-20 17:33:08 +0530

    anagram solution#3

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,28 +8,42 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
+// function anagrams(stringA, stringB) {
+    
+//     if(stringA.length !== stringB.length) return false;
+
+//     let stringAObj = getCharacterMap(stringA) ? getCharacterMap(stringA) : {};
+//     let stringBObj = getCharacterMap(stringB) ? getCharacterMap(stringB) : {};
+    
+//     for(let char in stringAObj){
+//         if(stringAObj[char] != stringBObj[char]) return false
+//     }
+
+//     return true;
+
+// }
+
+// /** SOLUTION# 2 */
+// function getCharacterMap(string){
+//     let mapObject = {};
+
+//     for(let char of string.replace(/[^\w]/g, '').toLowerCase()) mapObject[char] = mapObject[char] ? mapObject[char]++ : 1; 
+
+//     return mapObject;
+// }
+
+/** Solution #3 */
 function anagrams(stringA, stringB) {
     
-    if(stringA.length !== stringB.length) return false;
+    stringA = cleanUp(stringA) 
+    stringB = cleanUp(stringB);
 
-    let stringAObj = getCharacterMap(stringA) ? getCharacterMap(stringA) : {};
-    let stringBObj = getCharacterMap(stringB) ? getCharacterMap(stringB) : {};
-    
-    for(let char in stringAObj){
-        if(stringAObj[char] != stringBObj[char]) return false
-    }
-
-    return true;
-
+    if(stringA === stringB) return true;
+    return false;
 }
 
-/** SOLUTION# 2 */
-function getCharacterMap(string){
-    let mapObject = {};
-
-    for(let char of string.replace(/[^\w]/g, '').toLowerCase()) mapObject[char] = mapObject[char] ? mapObject[char]++ : 1; 
-
-    return mapObject;
+function cleanUp(string){
+    return string.replace(/[^\w]/g).toLowerCase().split('').sort().join();
 }
 
 module.exports = anagrams;
