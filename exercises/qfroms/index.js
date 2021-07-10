2e900357d022522cfa1f2535018e0e20f65b6486
Shivang Chaturvedi <shiva.chaturvedi91@gmail.com>
2021-07-10 18:15:47 +0530

    qfroms : Solution#1

--- exercises/qfroms/index.js
+++ exercises/qfroms/index.js
@@ -13,7 +13,39 @@
 //     q.remove(); // returns 2
 
 const Stack = require('./stack');
+class Queue {
+    constructor() {
+        this.s1 = new Stack();
+        this.s2 = new Stack();
+    }
 
-class Queue {}
+    add(item) {
+        this.s1.push(item)
+    }
+    remove() {
+        while (this.s1.peek()) {
+            this.s2.push(this.s1.pop())
+        }
+    
+        const record = this.s2.pop()
+
+        while (this.s2.peek()) {
+            this.s1.push(this.s2.pop())
+        }
+
+        return record
+    }
+    peek() {
+        while (this.s1.peek()) {
+            this.s2.push(this.s1.pop())
+        }
+        let record = this.s2.peek()
+
+        while (this.s2.peek()) {
+            this.s1.push(this.s2.pop())
+        }
+        return record
+    }
+}
 
 module.exports = Queue;
