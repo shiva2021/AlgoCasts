94b59b36133e2be40f6529faf05f50f78de18874
Shivang Chaturvedi <shiva.chaturvedi91@gmail.com>
2021-07-15 16:56:51 +0530

    fromlast: Solution #1

--- exercises/fromlast/index.js
+++ exercises/fromlast/index.js
@@ -11,6 +11,19 @@
 //    list.insertLast('d');
 //    fromLast(list, 2).data // 'b'
 
-function fromLast(list, n) {}
+function fromLast(list, n) {
+    let slow = list.head;
+    let fast = list.head;
+    let counter = 1
+    while (counter <= n && fast.next) {
+        fast = fast.next
+        counter++
+    }
+    while(fast.next){
+        slow = slow.next
+        fast = fast.next
+    }
+    return slow
+}
 
 module.exports = fromLast;
