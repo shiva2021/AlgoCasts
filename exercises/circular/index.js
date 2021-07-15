18f994d479ab3d3d431d2007d65a06d7c1624a54
Shivang Chaturvedi <shiva.chaturvedi91@gmail.com>
2021-07-15 15:34:36 +0530

    circular: solution #1

--- exercises/circular/index.js
+++ exercises/circular/index.js
@@ -12,6 +12,16 @@
 //   c.next = b;
 //   circular(l) // true
 
-function circular(list) {}
+function circular(list) {
+    let slow = list.head;
+    let fast = list.head;
+    
+    while(fast.next && fast.next.next){
+        if((fast.next.next === slow.next)) return true
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+    return false
+}
 
 module.exports = circular;
