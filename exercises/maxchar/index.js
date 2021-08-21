963c6ff8a7afdd7305667eee6fa8debcc0249d71
Shivang Chaturvedi <shiva.chaturvedi91@gmail.com>
2021-08-21 14:35:49 +0530

    maxchar ==> NEW SOLUTION

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,24 +5,39 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {
-  let obj = {};
-  let max = 0;
-  let maxChar = '';
+// function maxChar(str) {
+//   let obj = {};
+//   let max = 0;
+//   let maxChar = '';
 
-  for (let d of str) {
-    // !Object.prototype.hasOwnProperty.call(obj, d) ? obj[d] = 1 : obj[d]++
-    obj[d] = obj[d] + 1 || 1;
-  }
+//   for (let d of str) {
+//     // !Object.prototype.hasOwnProperty.call(obj, d) ? obj[d] = 1 : obj[d]++
+//     obj[d] = obj[d] + 1 || 1;
+//   }
 
-  for (let key in obj) {
-    if (obj[key] > max) {
-      max = obj[key];
-      maxChar = key;
+//   for (let key in obj) {
+//     if (obj[key] > max) {
+//       max = obj[key];
+//       maxChar = key;
+//     }
+//   }
+
+//   return maxChar
+// }
+
+
+/** SOLUTION #2 --> LATEST*/
+
+function maxChar(string) {
+    if(!string) return ""
+    let max = 1
+    let map = {},
+        maxstring;
+    for (let i = 0; i < string.length; i++) {
+        map[string[i]] = map[string[i]] ? map[string[i]]++ : 1;
+        if (map[string[i]] >= max) maxstring = string[i]
     }
-  }
-
-  return maxChar
+    return maxstring
 }
 
 module.exports = maxChar;
